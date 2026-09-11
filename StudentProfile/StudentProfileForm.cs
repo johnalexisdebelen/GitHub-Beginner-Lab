38b8238913d82030aff83ae5668cc8ee294f31f8
John Alexis De Belen <User@DeBelen>
2026-09-12 01:07:11 +0800

    Add StudentProfile Visual Studio project

---
+++ StudentProfile/StudentProfileForm.cs
@@ -0,0 +1,15 @@
+namespace StudentProfile
+{
+    public partial class StudentProfileForm : Form
+    {
+        public StudentProfileForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Student Profile: John Alexis De Belen");
+        }
+    }
+}
